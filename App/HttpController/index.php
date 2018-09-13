886b0fd65ccc19142a0ca18c3df4530dab83b894
Wataru Sendo <sendo_wataru@cyberagent.co.jp>
2018-09-13 21:56:43 +0900

    logger使ってみた

--- App/HttpController/index.php
+++ App/HttpController/index.php
@@ -1,10 +1,15 @@
 <?php
 namespace App\HttpController;
 use EasySwoole\Core\Http\AbstractInterface\Controller;
+use EasySwoole\Core\Component\Logger;
+
 class Index extends Controller
 {
   function index()
   {
+  		$logger = new Logger();
+
+  		$logger->log("loglogloglog");
       // TODO: Implement index() method.
       $this->response()->write('We Are Griphone.');
   }
